422abbcffb712bafdd6df12dd9b364532bc29db8
Michael Anderson <michanders@msn.com>
2018-02-17 19:27:25 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,4 +1,10 @@
 
 function hideWhenClicked(){
-  var hide = document.getElementById("hide_this")
+  var hide = document.getElementById("hide_this");
+  if (hide.style.display === "none"){
+    hide.style.display = "block";
+  }
+  else {
+    hide.style.display = "none";
+  }
 }
